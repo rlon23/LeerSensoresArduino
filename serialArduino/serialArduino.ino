c03e5e2ada5aae78074fd393265203f751cdc7d6
rlon23 <arlonydiaz@gmail.com>
2016-01-25 18:26:12 -0500

    Codigo
    Codigo y libreria, no sé como incluir libreria de arduino en GitHub

---
+++ serialArduino/serialArduino.ino
@@ -0,0 +1,30 @@
+#include <HX711.h>
+#define factorUno -766000
+HX711 scaleUno(A1, A0);
+HX711 scaleDos(A3, A2);
+HX711 scaleTres(A5, A4);
+HX711 scaleCuatro(A7, A6);
+void setup() {
+  Serial.begin(9600);
+  scaleUno.set_scale(factorUno);
+  scaleUno.tare();
+  scaleDos.set_scale(factorUno);
+  scaleDos.tare();
+  scaleTres.set_scale(factorUno);
+  scaleTres.tare();
+  scaleCuatro.set_scale(factorUno);
+  scaleCuatro.tare();
+}
+
+void loop() {
+  Serial.print(scaleUno.get_units(5)*1000,0);
+  Serial.print(" ");
+  Serial.print(scaleDos.get_units(5)*1000,0);
+  Serial.print(" ");
+  Serial.print(scaleTres.get_units(5)*1000,0);
+  Serial.print(" ");
+  Serial.print(scaleCuatro.get_units(5)*1000,0);
+  Serial.println();
+  Serial.print(" ");
+  delay(2000);
+}
